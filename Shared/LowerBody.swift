9a9a596ceffdb76c2f89e36df365839c599b6fc4
Alec <rh1397@msstate.edu>
2022-07-01 07:04:01 -0400

    test

--- Shared/LowerBody.swift
+++ Shared/LowerBody.swift
@@ -74,7 +74,7 @@ private var lowerMusclesGroup = [
     LowerMuscles(name: "Quadriceps"),
     LowerMuscles(name: "Adductors"),
     LowerMuscles(name: "Hamstrings"),
-    LowerMuscles(name: "Glutess"),
+    LowerMuscles(name: "Glutes"),
 ]
 
 
